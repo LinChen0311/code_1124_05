319344ea0017ccfdc5ecbccf09a1fc2773bf1d48
LinChen0311 <71689299+LinChen0311@users.noreply.github.com>
2020-11-24 15:55:09 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    if (Math.randomBoolean()) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
